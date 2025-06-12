d2dad1ac824430d82282541b5532cb80e3eaf1b5
Yukhio <yukhio.000@gmail.com>
2025-06-12 12:32:07 -0600

    add(index): Agreué una card con mis datos

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title="Mi Producto Increíble")
+    # Puedes reemplazar estos valores por lo que necesites
+    person = {
+        "name": "Enrique Yukhio Juárez Preciado",
+        "photo": "img/perfil.jpeg",
+        "job_title": "Desarrollador Full Stack",
+        "location": "Colima, Colima, México",
+        "contact_url": "https://www.linkedin.com/in/enrique-yukhio-ju%C3%A1rez-preciado-953b5b21a/"
+    }
+    return render_template("index.html", person=person)
 
 if __name__ == '__main__':
     app.run(debug=True)
